8804197f8b76cbe63a5ac6fd790bfb9422c211d1
Bala Luguja <bluguja@gmail.com>
2020-04-21 12:02:47 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,4 @@
-class EmailParser 
+class EmailAdresslParser 
   attr_accessor :name,:csv_emails
   
   def initialize(csv_emails)
@@ -12,12 +12,7 @@ def parse
   returnArr
 end
 
-#   def parse 
-#     csv_emails.split.collect do |address|
-#       address.split(',') 
-#     end.flatten.uniq 
-#   end 
-# end
+
 
 # emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
 # parser1 = EmailParser.new(emails1)
