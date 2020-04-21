87c67201454b547a49daec06e18083dcb819c556
Bala Luguja <bluguja@gmail.com>
2020-04-21 12:03:48 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,10 +14,10 @@ end
 
 
 
-# emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-# parser1 = EmailParser.new(emails1)
-# parser1.parse 
+emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
+parser1 = EmailParser.new(emails1)
+parser1.parse 
 
-# emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-# parser2 = EmailParser.new(emails2)
-# parser2.parse
+emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+parser2 = EmailParser.new(emails2)
+parser2.parse
