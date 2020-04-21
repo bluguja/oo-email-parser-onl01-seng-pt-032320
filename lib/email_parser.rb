25df0be6f87edfd952fad143796e11fd7654c53b
Bala Luguja <bluguja@gmail.com>
2020-04-21 12:07:19 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,4 @@
-class EmailAdresslParser 
+class EmailAddresslParser 
   attr_accessor :name,:csv_emails
   
   def initialize(csv_emails)
@@ -15,9 +15,9 @@ end
 
 
 emails1 = "avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"
-parser1 = EmailParser.new(emails1)
+parser1 = EmailAddressParser.new(emails1)
 parser1.parse 
 
 emails2 = "avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"
-parser2 = EmailParser.new(emails2)
+parser2 = EmailAddressParser.new(emails2)
 parser2.parse
